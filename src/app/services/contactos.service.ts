81fe702a74408d5d4d81e88906162892060ee124
priipalacio <prii.palacio@gmail.com>
2020-11-09 15:54:16 -0300

    1 file modified

--- src/app/services/contactos.service.ts
+++ src/app/services/contactos.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+  HttpParams
+} from "@angular/common/http";
+import { of } from "rxjs";
+import { Contactos } from "../models/contacto";
 
-@Injectable()
+@Injectable({ providedIn: "root" })
 export class ContactosService {
+  resourceUrl: string;
+  constructor(private httpClient: HttpClient) {
+    this.resourceUrl = "https://pav2.azurewebsites.net/api/contactos";
+  }
+  get(Nombre: string, Pagina: number) {
+    let params = new HttpParams();
+    if (Nombre != null) {
+      params = params.append("Nombre", Nombre);
+    }
+    params = params.append("Pagina", Pagina.toString());
 
-  constructor() { }
+    return this.httpClient.get(this.resourceUrl, { params: params });
+  }
 
+  post(obj: Contactos) {
+    return this.httpClient.post(this.resourceUrl, obj);
+  }
 }
