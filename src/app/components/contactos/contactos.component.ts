d5ca2054b8d0264d9084b4927004e0bb5d0b145f
priipalacio <prii.palacio@gmail.com>
2020-11-09 16:00:54 -0300

    1 file modified, 1 added

--- src/app/components/contactos/contactos.component.ts
+++ src/app/components/contactos/contactos.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ModalDialogService } from "../../services/modal-dialog.service";
+import { Contactos } from "../../models/contacto";
+import { ContactosService } from "../../services/contactos.service";
 
 @Component({
-  selector: 'app-contactos',
-  templateUrl: './contactos.component.html',
-  styleUrls: ['./contactos.component.css']
+  selector: "app-contactos",
+  templateUrl: "./contactos.component.html",
+  styleUrls: ["./contactos.component.css"]
 })
 export class ContactosComponent implements OnInit {
+  Titulo: "Contactos";
+  Acciones: {
+    A: "(Agregar)";
+    C: "(Consultar)";
+    L: "(Listado)";
+  };
+  Accion = "L"; //inicia con listado
 
-  constructor() { }
+  Mensajes = {
+    SD: " No se encontraron registros...",
+    RD: " Revisar los datos ingresados..."
+  };
 
-  ngOnInit() {
-  }
+  Lista: Contactos[] = [];
+  RegistrosTotal: number;
+  SinBusquedasRealizadas = true;
 
+  Pagina = 1;
+
+  FormFiltro: FormGroup;
+  FormReg: FormGroup;
+  submitted = false;
+
+  constructor(
+    public formBuilder: FormBuilder,
+    private modalDialogService: ModalDialogService,
+    private contactosService: ContactosService
+  ) {}
+
+  ngOnInit() {}
 }
