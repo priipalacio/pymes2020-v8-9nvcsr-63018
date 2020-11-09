5ccc8ad0c93abc18e6bcfed6e9c6fdb1662fcb29
priipalacio <prii.palacio@gmail.com>
2020-11-09 16:06:29 -0300

    1 file modified

--- src/app/components/contactos/contactos.component.ts
+++ src/app/components/contactos/contactos.component.ts
@@ -39,5 +39,41 @@ export class ContactosComponent implements OnInit {
     private contactosService: ContactosService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.FormFiltro = this.formBuilder.group({
+      Nombre: [""]
+    });
+    this.FormReg = this.formBuilder.group({
+      IdContacto: [0],
+      Nombre: [
+        "",
+        [Validators.required, Validators.minLength(4), Validators.maxLength(30)]
+      ],
+      FechaNacimiento: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            "(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}"
+          )
+        ]
+      ],
+      Telefono: [null, [Validators.required, Validators.pattern("[0-9]{1,10}")]]
+    });
+
+    this.getContactos();
+  }
+
+  getContactos() {
+    this.contactosService.get.subscribe((datos: Contactos[]) => {
+      this.Lista = datos;
+    });
+  }
+  Agregar() {
+    this.Accion = "A";
+    this.FormReg.reset({ Activo: true });
+    this.submitted = false;
+    //this.FormReg.markAsPristine();
+    this.FormReg.markAsUntouched();
+  }
 }
